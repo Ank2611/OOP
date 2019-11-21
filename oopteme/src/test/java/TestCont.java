730aa79c02735f35b4d5305384282203ba215546
Anca <mank1167@gmail.com>
2019-11-21 23:37:34 +0200

    Adaugare metoda de Test

---
+++ oopteme/src/test/java/TestCont.java
@@ -0,0 +1,19 @@
+import Tema4.Cont;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCont {
+    Cont c1 = new Cont(1, 100);
+
+    @Test
+    public void depozitTest() {
+        //System.out.println("depozit " + c1.depozit(250));
+        Assert.assertEquals(350.0, c1.depozit(250), 0.0);
+    }
+
+    @Test
+    public void retragTest() {
+        //System.out.println("depozit " + c1.depozit(250));
+        Assert.assertEquals(350.0, c1.depozit(150), 0.0);
+    }
+}
