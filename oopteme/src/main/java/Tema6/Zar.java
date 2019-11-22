83e44ef4a644ee565aee88906458c18ca64fbcf6
Anca <mank1167@gmail.com>
2019-11-22 22:15:03 +0200

    Creare clasa Zar

---
+++ oopteme/src/main/java/Tema6/Zar.java
@@ -0,0 +1,17 @@
+package Tema6;
+
+public class Zar {
+    private int[] values = {1,2,3,4,5,6};
+
+    public int[] getValues() {
+        return values;
+    }
+
+    public int getDiceValue(int n){
+        int result = 0;
+        for (int i = 0; i < 6 ; i++) {
+            result = values[n];
+        }
+        return result;
+    }
+}
